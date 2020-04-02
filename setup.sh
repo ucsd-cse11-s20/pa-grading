41e28288e04ca1193825505fafc95d1676d7f9bd
CharlesJiangtc <charlesjinagtc@gmail.com>
2020-04-01 19:55:00 -0700

    modified setup.sh and run_autograder for cse11-s20

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 
 # Install any dependencies you need here (you can run more than apt-get, and
 # this script runs as root)
-apt-get -y install openjdk-8-jdk valgrind
+apt-get -y install openjdk-14-jdk valgrind
 
 
 # This last line must stay to avoid ssh errors
