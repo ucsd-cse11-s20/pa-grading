9fd60ab1af8abb988832e89049eb81e7e8b01463
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-04-05 17:51:00 -0700

    made build-for-gradescope fail without ssh key

--- build-for-gradescope.sh
+++ build-for-gradescope.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ $# -ne 1 ]; 
-    then echo "Provide the path to your Github ssh key as the first parameter"
+if [ $# -ne 1 ]; then 
+	echo "Provide the path to your Github ssh key as the first parameter"
+	exit 1
 fi
 
 cp $1 id_rsa
