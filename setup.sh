7862a8e8a6cafa191446e50272ebb1c9c8cdf212
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-04-05 18:03:24 -0700

    fixed java version

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # Install any dependencies you need here (you can run more than apt-get, and
 # this script runs as root)
 apt-get update
-apt-get -y install openjdk-14-jdk
+apt-get -y install openjdk-11-jdk
 
 
 # This last line must stay to avoid ssh errors
